918256e319c78e8ac8a72c635cd9db5d848c78e7
digitalsleuth <corey@digitalsleuth.ca>
2024-01-02 23:39:28 +0000

    Fix race condition

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup
 
 setup(
     name="peepdf-3",
-    version="2.2.0",
+    version="2.3.0-rc1",
     author="Jose Miguel Esparza, Corey Forman",
     license="GNU GPLv3",
     url="https://github.com/digitalsleuth/peepdf-3",
     description=("The original peepdf, ported to Python 3, and packaged in a setup"),
     install_requires=[
         "requests",
-        "pypdf",
         "jsbeautifier",
         "colorama",
         "Pillow",
-        "pythonaes",
         "pylibemu",
+        "pythonaes",
         "lxml",
     ],
     entry_points={
